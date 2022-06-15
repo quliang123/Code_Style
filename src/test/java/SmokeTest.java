453083009d3ad8720cc36d3e385d6d0b7d07c856
quliang <529075990@qq.com>
2022-06-15 11:16:47 +0800

    Initial commit

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @program: Code_Style
+ * @description:
+ * @author: quliang
+ * @create: 2022-06-15 10:10
+ **/
+
+public class SmokeTest {
+
+    @Test
+    void one_plus_one_should_equal_two(){
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
